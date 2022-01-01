a6b1e3e905938c1b62e9ef77d0e4a196bbcf0f56
Adam Byrd <adam1.byrd@gmail.com>
2021-12-31 18:11:39 -0800

    Add visualizer entry point - This is the minimum necessary to create a loadable module - The module definition file is necessary because ATL forward declares the functions we need to export and uses the STDAPI macro. This macro prevents us from using __declspec(dllexport). Doing so causes an error because the declaration and definition have different signatures.

---
+++ ecs-visualizer/dllmain.cpp
@@ -0,0 +1,29 @@
+// -------------------------------------------------------------------------------------------------
+// Visual Studio Extensibility
+
+#define _ATL_FREE_THREADED
+
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlctl.h>
+using namespace ATL;
+
+class ECSVisualizerModule : public CAtlDllModuleT<ECSVisualizerModule> {};
+extern class ECSVisualizerModule _AtlModule;
+ECSVisualizerModule _AtlModule;
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved);
+}
+
+STDAPI DllCanUnloadNow(void)
+{
+    return _AtlModule.DllCanUnloadNow();
+}
+
+STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
+{
+    return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
+}
