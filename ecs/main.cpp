96c948df23c5af4d7c5bbc590311c27aa2dbc558
Adam Byrd <abyrd@phxlabs.ca>
2022-01-05 09:51:14 -0800

    Add testing for various ways data will show up in the watch window

--- ecs/main.cpp
+++ ecs/main.cpp
@@ -1,5 +1,15 @@
 #include "types.h"
 
+Archetype  vFunc(Archetype* archetype) { return *archetype; }
+Archetype& rFunc(Archetype* archetype) { return *archetype; }
+Archetype* pFunc(Archetype* archetype) { return  archetype; }
+
+using vArray = Archetype[1];
+using pArray = Archetype*[1];
+
+const vArray& avFunc(Archetype* archetype) { static vArray arr; arr[0] = *archetype; return arr; }
+const pArray& apFunc(Archetype* archetype) { static pArray arr; arr[0] =  archetype; return arr; }
+
 int main()
 {
 	Component component = {};
@@ -9,6 +19,21 @@ int main()
 	archetype.entityCount = 1;
 	archetype.componentType = "Component";
 	archetype.componentArray = &component;
+
+	// Test Various Scenarios
+	Archetype  vArchetype =  archetype;
+	Archetype& rArchetype =  archetype;
+	Archetype* pArchetype = &archetype;
+
+	Archetype  avArchetype[] = {  archetype };
+	Archetype* apArchetype[] = { &archetype };
+
+	vFunc(&archetype);
+	rFunc(&archetype);
+	pFunc(&archetype);
+
+	avFunc(&archetype);
+	apFunc(&archetype);
 
 	return 0;
 }
