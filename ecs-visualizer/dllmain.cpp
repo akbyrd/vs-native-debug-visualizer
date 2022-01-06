5244a057e33fc113aa89ba1d3e873427911c99a6
Adam Byrd <abyrd@phxlabs.ca>
2022-01-06 10:30:04 -0800

    Add stub for visualizer service

--- ecs-visualizer/dllmain.cpp
+++ ecs-visualizer/dllmain.cpp
@@ -27,3 +27,87 @@ STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv)
 {
 	return _AtlModule.DllGetClassObject(rclsid, riid, ppv);
 }
+
+// -------------------------------------------------------------------------------------------------
+// Concord Extensibility
+
+#include <vsdebugeng.h>
+#include <vsdebugeng.templates.h>
+using namespace Microsoft::VisualStudio::Debugger;
+using namespace Microsoft::VisualStudio::Debugger::Evaluation;
+#pragma comment(lib, "VSDebugEng.lib")
+
+#include "ecs-visualizer.Contract.h"
+
+class ATL_NO_VTABLE ECSVisualizerService :
+    public ECSVisualizerServiceContract,
+    public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<ECSVisualizerService, &ECSVisualizerServiceContract::ClassId>
+{
+protected:
+    ECSVisualizerService() {}
+    ~ECSVisualizerService() {}
+
+public:
+    DECLARE_NO_REGISTRY();
+    DECLARE_NOT_AGGREGATABLE(ECSVisualizerService);
+
+    HRESULT STDMETHODCALLTYPE EvaluateVisualizedExpression(
+        _In_            DkmVisualizedExpression* pVisualizedExpression,
+        _Deref_out_opt_ DkmEvaluationResult**    ppResultObject
+    )
+    {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE UseDefaultEvaluationBehavior(
+        _In_            DkmVisualizedExpression* pVisualizedExpression,
+        _Out_           bool*                    pUseDefaultEvaluationBehavior,
+        _Deref_out_opt_ DkmEvaluationResult**    ppDefaultEvaluationResult
+    )
+    {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetChildren(
+        _In_        DkmVisualizedExpression*                 pVisualizedExpression,
+        _In_        UINT32                                   InitialRequestSize,
+        _In_        DkmInspectionContext*                    pInspectionContext,
+        _Out_       DkmArray<DkmChildVisualizedExpression*>* pInitialChildren,
+        _Deref_out_ DkmEvaluationResultEnumContext**         ppEnumContext
+    )
+    {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetItems(
+        _In_  DkmVisualizedExpression*                 pVisualizedExpression,
+        _In_  DkmEvaluationResultEnumContext*          pEnumContext,
+        _In_  UINT32                                   StartIndex,
+        _In_  UINT32                                   Count,
+        _Out_ DkmArray<DkmChildVisualizedExpression*>* pItems
+    )
+    {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE SetValueAsString(
+        _In_            DkmVisualizedExpression* pVisualizedExpression,
+        _In_            DkmString*               pValue,
+        _In_            UINT32                   Timeout,
+        _Deref_out_opt_ DkmString**              ppErrorText
+    )
+    {
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetUnderlyingString(
+        _In_            DkmVisualizedExpression* pVisualizedExpression,
+        _Deref_out_opt_ DkmString**              ppStringValue
+    )
+    {
+        return E_NOTIMPL;
+    }
+};
+
+OBJECT_ENTRY_AUTO(ECSVisualizerService::ClassId, ECSVisualizerService)
